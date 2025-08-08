0535d491ba0afda0b56d0d7cfd6be697621d78bd
S-Gauri-23 <76697678+S-Gauri-23@users.noreply.github.com>
2025-08-08 15:43:17 +0530

    made a few updates

--- app.py
+++ app.py
@@ -2,27 +2,12 @@ from flask import Flask,url_for,render_template,request
 from bs4 import BeautifulSoup
 import requests
 
-
-# url = "https://www.businesstoday.in/technology/news"
-# req = requests.get(url)
-# soup = BeautifulSoup(req.content, 'html.parser')
-    #outerData = soup.find_all("div",class_="widget-listing",limit=6)
-    #print(outerData)
-# finalNews=""
-# for data in soup.find_all("div",class_="Section_widget_listing__Bf6q8",limit=6):
-#     # news = data
-#     news=data.div.div.a["title"]
-#     finalNews += '\u2022 '+news+'\n'
-# print(finalNews)
-
 app = Flask(__name__)
 @app.route('/',methods=["GET","POST"])
 def index():
     url = "https://www.businesstoday.in/technology/news"
     req = requests.get(url)
     soup = BeautifulSoup(req.content, 'html.parser')
-    #outerData = soup.find_all("div",class_="widget-listing",limit=6)
-    #print(outerData)
     finalNews=""
     for data in soup.find_all("div",class_="Section_widget_listing__Bf6q8",limit=6):
         news=data.div.div.a["title"]
